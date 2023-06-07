e5ea2c0dd78d02a05730758a5b4799fadb2ff21b
Emete Gift <emeteokeoghene@gmail.com>
2023-06-07 15:41:33 +0100

    Bincom ass

---
+++ app.py
@@ -0,0 +1,27 @@
+"""
+    1st Contact Section - Assignment
+Install the following packages with pip
+
+psycopg2,
+
+mysqldb,
+
+urllib,
+
+urllib2,
+
+urllib3,
+
+scipy,
+
+numpy,
+
+pandas,
+
+matplotlib
+"""
+
+## Solution to the Assignment
+"""
+    Solutions are in requirements.txt file
+"""
